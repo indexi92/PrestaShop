a2b5002eff889a9329b176aba7be3b9076048c40
indexi92 <klicicervin@hotmail.com>
2022-08-15 16:44:37 +0200

    Add exercice 3

---
+++ cypress/e2e/Exercice3.js
@@ -0,0 +1,10 @@
+let n1 = 1;
+let n2 = 1 * 2;
+let n3 = 1 * 2 * 3;
+let n4 = 1 * 2 * 3 * 4;
+let n5 = 1 * 2 * 3 * 4 * 5;
+let n6 = 1 * 2 * 3 * 4 * 5 * 6;
+
+result = n1 + n2 + n3 + n4 + n5 + n6;
+
+console.log(result);
